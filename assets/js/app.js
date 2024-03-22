d0994a6e4bb61d0d4850bcb5d8bda5cc1fe26b91
Zehra Saraç <zehrassarac@gmail.com>
2024-03-22 09:24:51 +0300

    22.03

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,15 @@ const navMenu = document.querySelector('.nav-menu');
 hamMenu.addEventListener('click', ()=>{
     hamMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
-})
+});
 
+
+const questions = document.querySelectorAll(".question");
+
+console.log(questions);
+
+questions.forEach((question) => {
+    question.addEventListener("click",function () {
+        question.classList.toggle("active");
+    });
+});
